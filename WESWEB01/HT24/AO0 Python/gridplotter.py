4d70322265419c6a824787483e6025d24e7075b9
Laxen <maximiliam.froste@outlook.com>
2024-09-02 12:05:10 +0200

    Gridplotter Cirkel

--- WESWEB01/HT24/AO0 Python/gridplotter.py
+++ WESWEB01/HT24/AO0 Python/gridplotter.py
@@ -2,18 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = [
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,]
 ]
 
 # SKRIV DIN LÖSNING HÄR
@@ -65,23 +65,41 @@ for list in range(len(data)):
 #                data[list][num] = 1
 
 # 6
-dataRange = (len(data))
-midPoint = dataRange//2
+#dataRange = (len(data))
+#midPoint = dataRange//2
+#
+#for list in range(len(data)):
+#    for num in range(len(data[list])):
+#
+#        if num  > 3 and num < 8 or list > 3 and list < 8:
+#            if list == 0 or list == len(data)-1 or num == 0 or num == len(data)-1:
+#                data[list][num] = 1
+#
+#        if num < 10 and num > 1 and num != midPoint or list < 10 and list > 1 and list != midPoint:
+#            if list != 0 and list != 11 and num != 0 and num != 11: #and list != 5 and num != 5 and list != 6 and num != 6:
+#                    data[list][num] = 1
 
-for list in range(len(data)):
-    for num in range(len(data[list])):
+def setSize(num):
+    global data
+    data = []
+    for x in range(num):
+        data.append([0]*num)
 
-        if num  > 3 and num < 8 or list > 3 and list < 8:
-            if list == 0 or list == len(data)-1 or num == 0 or num == len(data)-1:
-                data[list][num] = 1
+setSize(100)
 
-        if num < 10 and num > 1 and num != midPoint or list < 10 and list > 1 and list != midPoint:
-            if list != 0 and list != 11 and num != 0 and num != 11 and list != 5 and num != 5 and list != 6 and num != 6:
-                    data[list][num] = 1
-        
-        if list > 6 and list <10:
-            if num > 6 and num < 9:
-                data[list][num] = 0
+# Ritar cirkeln
+radius = (len(data)-1)/2
+for x in range(len(data)):
+    for y in range(len(data[list])):
+        if (x-radius)**2 + (y-radius)**2 <= radius**2:
+            data[x][y] = 1
+
+# Rensar cirkeln
+radius = radius - 1.5
+for x in range(len(data)):
+    for y in range(len(data[list])):
+        if (x-1.5-radius)**2 + (y-1.5-radius)**2 <= radius**2:
+            data[x][y] = 0
 # ---------------------
 
 grid = np.array(data)
@@ -89,8 +107,8 @@ fig, ax = plt.subplots()
 cmap = plt.get_cmap('Greys')
 ax.imshow(grid, cmap=cmap)
 
-ax.set_xticks(np.arange(-0.5, 12, 1), minor=True)
-ax.set_yticks(np.arange(-0.5, 12, 1), minor=True)
+#ax.set_xticks(np.arange(-0.5, len(data), 1), minor=True)
+#ax.set_yticks(np.arange(-0.5, len(data), 1), minor=True)
 ax.grid(which='minor', color='black', linestyle='-', linewidth=2)
 
 ax.tick_params(which='both', size=0, labelbottom=False, labelleft=False)
